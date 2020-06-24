81605d7680c42c6411596e2a41dbcb0a001c94fc
David Zhang <kiteabpl@outlook.com>
2020-06-24 22:17:13 +0800

    change pg ctrl keys

--- config.h
+++ config.h
@@ -225,7 +225,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_e,   kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_e,           kscrolldown,    {.i = -1} },
 };
 
 /*
